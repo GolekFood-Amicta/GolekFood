b79481855b7ae8aaadb3591bc752910c9763eb13
Ma'mur Zaky Nurrokhman <mamurzakynurrokhman@gmail.com>
2023-06-11 19:24:55 +0700

    membuat halaman sign up dan perbaikan tampilan

--- src/components/GolekFoodsApp.js
+++ src/components/GolekFoodsApp.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function GolekFoodsApp() {
   return (
-    <div className="GolekFoodsApp">
-      <SignIn />
-    </div>
+    // <div className="GolekFoodsApp">
+    //   <SignIn />
+    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-class SignIn extends React.Component {
+class SignUp extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            name: '',
             email: '',
-            password: ''
+            password: '',
+            confirmPassword: ''
         }
 
+        this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
         this.onEmailChangeEventHandler = this.onEmailChangeEventHandler.bind(this);
         this.onPasswordChangeEventHandler = this.onPasswordChangeEventHandler.bind(this);
+        this.onConfirmPasswordChangeEventHandler = this.onConfirmPasswordChangeEventHandler.bind(this);
+    }
+
+    onNameChangeEventHandler(event) {
+        this.setState(() => {
+            return {
+                name: event.target.value,
+            }
+        })
     }
 
     onEmailChangeEventHandler(event) {
@@ -30,9 +42,17 @@ class SignIn extends React.Component {
         })
     }
 
+    onConfirmPasswordChangeEventHandler(event) {
+        this.setState(() => {
+            return {
+                confirmPassword: event.target.value,
+            }
+        })
+    }
+
     render() {
         return (
-            <div className="flex" >
+            <div className="flex">
                 <div className="w-full h-screen">
                     <div className="fixed bg-black w-2/5 h-5/6 mt-16 ml-16 rounded-xl" />
                     <div className="fixed bg-GF-grey w-2/5 h-5/6 mt-8 ml-8 rounded-xl" />
@@ -42,24 +62,27 @@ class SignIn extends React.Component {
                         <img src={require('../../assets/logo.png')} alt="logo" className="w-32" />
                     </div>
                     <div className="w-11/12">
-                        <p className="text-xl font-bold mb-4">Welcome to GolekFoods, <br />Sign In to Continue </p>
-                        <p className="text-sm font-medium">Don’t have an account? <Link to={"/SignUp"}><span className="underline hover:font-semibold hover:text-GF-green">Create an Account</span></Link> <br /> It takes less than a minute.</p>
+                        <p className="text-xl font-bold mb-4">Welcome to GolekFoods, <br />Sign Up to Continue </p>
+                        <p className="text-sm font-medium">Get started on your journey to better health with GolekFoods.</p>
                         <div className="h-16" />
+
+                        <p className="text-sm font-medium mb-2">Name</p>
+                        <input type="text" value={this.state.name} onChange={this.onNameChangeEventHandler} className="border-2 border-black p-2 rounded-xl w-full" />
+                        <div className="h-8" />
+
                         <p className="text-sm font-medium mb-2">Email</p>
                         <input type="email" value={this.state.email} onChange={this.onEmailChangeEventHandler} className="border-2 border-black p-2 rounded-xl w-full" />
                         <div className="h-8" />
+
                         <p className="text-sm font-medium mb-2">Password</p>
                         <input type="password" value={this.state.password} onChange={this.onPasswordChangeEventHandler} className="border-2 border-black p-2 rounded-xl w-full" />
                         <div className="h-8" />
-                        <div className="flex justify-between">
-                            <div className="flex items-center space-x-2">
-                                <input type="checkbox" className="accent-GF-green w-5 h-5 border-2 border-black rounded-xl focus:outline-none hover:border-GF-green hover:ring-2 hover:ring-GF-green" />
-                                <span className="text-xs">Remember me</span>
-                            </div>
-                            <a href="#" className="text-xs hover:font-semibold hover:text-GF-green">Forgot Password?</a>
-                        </div>
+
+                        <p className="text-sm font-medium mb-2">Confirm Password</p>
+                        <input type="password" value={this.state.confirmPassword} onChange={this.onConfirmPasswordChangeEventHandler} className="border-2 border-black p-2 rounded-xl w-full" />
                         <div className="h-8" />
-                        <button className="w-full bg-black text-white py-4 rounded-xl hover:bg-GF-green">Sign In</button>
+
+                        <button className="w-full bg-black text-white py-4 rounded-xl hover:bg-GF-green">Sign Up</button>
                     </div>
                 </div>
             </div>
@@ -67,4 +90,4 @@ class SignIn extends React.Component {
     }
 }
 
-export default SignIn;
+export default SignUp;
