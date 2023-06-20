42ba90ce7499d6adf3c05683fd419f6297083ed1
Ma'mur Zaky Nurrokhman <mamurzakynurrokhman@gmail.com>
2023-06-20 22:06:44 +0700

    menambahkan elemen desain pada bagian headline di halaman home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import NavBar from "../NavBar";
 import vector from "../../assets/vector/vector-home.svg";
+import headlineSwirl from "../../assets/headline-swirl.svg";
 
 function Home() {
     return (
@@ -9,21 +11,27 @@ function Home() {
 
             {/* Headline */}
             <div className="flex">
-                <div className="w-1/2 pl-16 flex flex-col justify-center space-y-8">
+                <div className="w-1/2 h-screen pl-16 flex flex-col justify-center space-y-8">
                     <div>
                         <p className="font-bold text-7xl text-GF-grey"><span className="text-GF-green">Solusi</span> Cerdas,</p>
                         <p className="font-bold text-6xl text-GF-grey">untuk menemukan</p>
                         <p className="font-bold text-5xl text-GF-grey">makanan sehat dan tepat</p>
                     </div>
                     <p className="font-medium text-xl text-GF-light-grey">Lebih Banyak Pilihan, Lebih Banyak Kesehatan<br />GolekFoods Memiliki Semua yang Anda Butuhkan.</p>
-                    <button className="bg-GF-green w-fit rounded-3xl px-6 py-4">
-                        <p className="font-semibold text-lg text-white">Discover Foods</p>
-                    </button>
+                    <Link to={"/Discover"}>
+                        <button className="bg-GF-green w-fit rounded-3xl px-6 py-4">
+                            <p className="font-semibold text-lg text-white">Discover Foods</p>
+                        </button>
+                    </Link>
                 </div>
-                <div className="w-1/2">
-                    <img src={vector} alt="vector" className="w-11/12 m-auto" />
+                <div className="w-1/2 h-screen flex justify-center">
+                    <img src={headlineSwirl} alt="swirl" className="absolute top-0 right-0" />
+                    <img src={vector} alt="vector" className="absolute w-5/12" />
                 </div>
             </div>
+
+            {/* Why Choose Us */}
+
         </div>
     );
 }
