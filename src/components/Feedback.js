64ddd581bfff46f6f0685524319055ba506e3751
Ma'mur Zaky Nurrokhman <mamurzakynurrokhman@gmail.com>
2023-06-20 20:02:05 +0700

    memperbaiki tampilan feedback

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -11,8 +11,8 @@ class Feedback extends React.Component {
                 <div className="flex  justify-end">
                     <button onClick={this.closeFeedback} className="text-2xl">&times;</button>
                 </div>
-                <p className="text-center font-semibold text-lg">Feedback</p>
-                <p className="text-center my-8">We value your opinion, help us improve with your <span className="font-bold">feedback</span></p>
+                <p className="text-center font-semibold text-lg text-GF-green">Feedback</p>
+                <p className="text-center text-GF-grey my-8">We value your opinion, help us improve with your <span className="font-bold">feedback</span></p>
 
                 <textarea placeholder="Input your feedback here..." className="w-full h-1/2 resize-none border-2 border-black rounded-xl p-4 accent-transparent" />
                 <div className="flex justify-center mt-8">
