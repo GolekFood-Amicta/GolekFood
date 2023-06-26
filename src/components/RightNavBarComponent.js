b92e0db8b178b7a84cdaf78a46b09ebd9bf9d10c
Ma'mur Zaky Nurrokhman <mamurzakynurrokhman@gmail.com>
2023-06-26 20:22:32 +0700

    menambahkan dropdown saat mengklik user component pada navbar

--- src/components/RightNavBarComponent.js
+++ src/components/RightNavBarComponent.js
@@ -5,9 +5,11 @@ import userData from "../data/UserData";
 import api from "../api/api";
 import { useState } from "react";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 function RightNavBarComponent() {
     const [user, setUser] = useState(null);
+    const [dropdown, setDropdown] = useState(false);
 
     useEffect(() => {
         async function getUser() {
@@ -17,6 +19,10 @@ function RightNavBarComponent() {
         getUser();
         console.log(user);
     }, [])
+
+    const showDropdown = event => {
+        setDropdown(dropdown => !dropdown);
+    }
 
     if (!user) {
         return (
@@ -29,7 +35,21 @@ function RightNavBarComponent() {
     return (
         <>
             {
-                localStorage.getItem('token') ? <UserComponent name={user.data.name} image={user.data.avatar === 'default-profile.png' ? userData[0].photo : ''} /> : <SignInButton />
+                localStorage.getItem('token') ? <UserComponent name={user.data.name} image={user.data.avatar === 'default-profile.png' ? userData[0].photo : ''} onClick={showDropdown} /> : <SignInButton />
+            }
+            {
+                localStorage.getItem('token') ? <div className={`absolute ${dropdown ? 'block' : 'hidden'} flex flex-col justify-start top-20 right-16 w-40 bg-black bg-opacity-50 text-white rounded-xl`}>
+                    <button className="hover:bg-white hover:bg-opacity-50 transition h-10 rounded-xl">
+                        <Link to={'/Profile'}>
+                            Profil
+                        </Link>
+                    </button>
+                    <button className="hover:bg-red-500 hover:bg-opacity-50 transition h-10 rounded-xl">
+                        <Link to={'/'}>
+                            Keluar
+                        </Link>
+                    </button>
+                </div> : null
             }
         </>
     );
