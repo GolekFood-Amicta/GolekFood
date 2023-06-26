673085211ae54684c52147de32bc1d1510fa8b51
Ma'mur Zaky Nurrokhman <mamurzakynurrokhman@gmail.com>
2023-06-26 21:57:15 +0700

    perbaikan tampilan food item, mengubah ikon favorit, dan menambahkan logika saat ikon favorit diklik

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,11 +1,20 @@
 import React from "react";
-import favorite_icon from "../assets/favorite_icon.svg"
+import favorite_icon from "../assets/favorite.svg"
+import unfavorite_icon from "../assets/unfavorite.svg"
+import { useState } from "react";
 
 function FoodItem({ foodName, foodImage, fatValue, calValue, proValue, carboValue }) {
+    const [isFavorite, setFavorite] = useState(false);
+
+    const favorite = event => {
+        event.preventDefault();
+        setFavorite(isFavorite => !isFavorite);
+    }
+
     return (
         <div className="relative w-72 h-96">
-            <button className="absolute right-0 m-4">
-                <img src={favorite_icon} alt="favorite" />
+            <button onClick={favorite} className="absolute right-0 m-4">
+                <img src={isFavorite ? favorite_icon : unfavorite_icon} alt="favorite" className="w-10" />
             </button>
             <div className="absolute w-full h-1/2 bottom-0 rounded-2xl bg-black bg-opacity-50 backdrop-blur text-white">
                 <p className="pt-2 font-medium text-lg text-center">{foodName}</p>
@@ -20,7 +29,7 @@ function FoodItem({ foodName, foodImage, fatValue, calValue, proValue, carboValu
                     <span className="col-span-4">Karbohidrat</span>
                 </div>
             </div>
-            <img src={foodImage} alt="food" className="w-72 h-96 object-cover  border-2 border-[#5F7161E5] border-opacity-90 rounded-2xl" />
+            <img src={foodImage} alt="food" className="w-72 h-96 object-cover rounded-2xl" />
         </div>
     );
 }
