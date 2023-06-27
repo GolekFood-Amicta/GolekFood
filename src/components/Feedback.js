fca5c172a8689c0f0d459a2135e04ddaf7e2c960
Ma'mur Zaky Nurrokhman <mamurzakynurrokhman@gmail.com>
2023-06-27 13:49:09 +0700

    menambahkan kolom subjek pada feedback

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,23 +1,54 @@
 import React from "react";
 
 class Feedback extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            subject: '',
+            content: ''
+        }
+
+        this.onSubjectChangeEventHandler = this.onSubjectChangeEventHandler.bind(this);
+        this.onContentChangeEventHandler = this.onContentChangeEventHandler.bind(this);
+    }
+
     closeFeedback = () => {
         this.props.toggle();
+    }
+
+    onSubjectChangeEventHandler(event) {
+        this.setState(() => {
+            return {
+                subject: event.target.value,
+            }
+        })
+    }
+
+    onContentChangeEventHandler(event) {
+        this.setState(() => {
+            return {
+                content: event.target.value,
+            }
+        })
     }
 
     render() {
         return (
             <div className="z-[999] shadow fixed m-auto top-0 bottom-0 right-0 left-0 w-1/2 h-5/6 rounded-3xl p-8 bg-white">
-                <div className="flex  justify-end">
+                <div className="flex justify-end">
                     <button onClick={this.closeFeedback} className="text-2xl">&times;</button>
                 </div>
                 <p className="text-center font-semibold text-lg text-GF-green">Masukan</p>
                 <p className="text-center my-8">Kami sangat menghargai saran maupun kritik anda.<br />Bantu kami meningkatkan GolekFoods dengan mengirim masukan.</p>
 
-                <textarea placeholder="Tulis masukanmu tentang GolekFoods disini..." className="w-full h-1/2 resize-none border-2 border-black rounded-xl p-4 accent-transparent" />
-                <div className="flex justify-center mt-8">
-                    <button className="bg-GF-green text-white rounded-xl py-4 w-1/2">Kirim Masukan</button>
-                </div>
+                <form className="h-2/3 flex flex-col justify-around">
+                    <input value={this.state.subject} onChange={this.onSubjectChangeEventHandler} placeholder="Subjek" className="w-full border-2 border-black rounded-xl p-4 accent-transparent" />
+                    <textarea value={this.state.content} onChange={this.onContentChangeEventHandler} placeholder="Tulis masukanmu tentang GolekFoods disini..." className="w-full h-1/2 resize-none border-2 border-black rounded-xl p-4 accent-transparent" />
+                    <div className="flex justify-center">
+                        <button className="bg-GF-green text-white rounded-xl py-4 w-1/2">Kirim Masukan</button>
+                    </div>
+                </form>
             </div>
         );
     }
