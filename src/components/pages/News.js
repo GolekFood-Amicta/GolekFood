9df11fe5520fdaead8175d5cddad1d49260d2fa3
Ma'mur Zaky Nurrokhman <mamurzakynurrokhman@gmail.com>
2023-06-29 11:28:48 +0700

    integrasi halaman News dengan API dan perbaikan tampilan pada halaman News

--- src/components/pages/News.js
+++ src/components/pages/News.js
@@ -1,19 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../NavBar";
 import vector from "../../assets/vector/vector-news.svg";
-import NewsPaginate from "../NewsPaginate";
+import api from "../../api/api";
+import NewsItem from "../NewsItem";
 
 function News() {
+    let [news, setNews] = useState(null);
+    let [page, setPage] = useState(1);
+    const pageIndex = [];
+
+    useEffect(() => {
+        async function getNews() {
+            const response = await api.get(`news?page=${page}`);
+            setNews(response.data);
+        }
+        getNews();
+    }, [page]);
+
+    const next = () => {
+        setPage(page = page + 1);
+
+    }
+
+    const prev = () => {
+        setPage(page = page - 1);
+    }
+
+    if (news) {
+        for (let index = 1; index <= news.data.last_page; index++) {
+            pageIndex.push(index);
+        }
+    }
+
+    console.log(`page = ${page}`);
+
     return (
         <div className="mb-8">
             <NavBar />
             <div className="flex">
                 <div className="w-1/2 pl-16 pr-8 pt-8 pb-8">
                     <p className="text-sm w-1/2">Temukan berita dan informasi <span className="font-semibold">makanan sehat</span>, menarik, dan terbaru.</p>
-                    <NewsPaginate newsItemsPerPage={4} />
+                    {
+                        news && (
+                            <>
+                                {
+                                    news.data.data.map((item) => (
+                                        <NewsItem
+                                            key={item.id}
+                                            title={item.title}
+                                            body={item.body}
+                                            // newsImage={item.image}
+                                            newsImage={'https://picsum.photos/seed/picsum/300'}
+                                            link={'DetailNews'}
+                                            author={'Anas Fikri Hanif'}
+                                            authorImage={'https://picsum.photos/200'}
+                                        />
+                                    ))
+                                }
+                                <div className="flex space-x-10 w-full fixed left-0 bottom-0 bg-white pl-16 py-4">
+                                    {
+                                        news.data.current_page !== 1 ? <button onClick={prev} className="text-sm font-medium">Sebelumnya</button> : null
+                                    }
+                                    {
+                                        pageIndex.map((pageNumber, index) => (
+                                            <button onClick={() => setPage(page = pageNumber)} className={`text-sm font-medium px-4 py-2 rounded-xl ${page === index + 1 ? 'border-2 border-black' : ''}`}>{pageNumber}</button>
+                                        ))
+                                    }
+                                    {
+                                        news.data.current_page !== news.data.last_page ? <button onClick={next} className="text-sm font-medium">Selanjutnya</button> : null
+                                    }
+                                </div>
+                            </>
+                        )
+                    }
                 </div>
                 <div className="w-1/2 px-8">
-                    <img src={vector} alt="vector" className="fixed w-2/2" />
+                    <img src={vector} alt="vector" className="fixed w-5/12 right-10" />
                 </div>
             </div>
         </div>
