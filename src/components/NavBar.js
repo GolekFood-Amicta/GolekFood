381b5d66564879538ceda63bfce7644d377ffdee
Ma'mur Zaky Nurrokhman <mamurzakynurrokhman@gmail.com>
2023-06-30 22:19:59 +0700

    menambahkan peringatan ketika pengguna yang belum signin mencoba menggunakan fitur temukan makanan

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,27 +2,56 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "./Logo";
 import RightNavBarComponent from "./RightNavBarComponent";
+import { useState } from "react";
 
 function NavBar() {
+    const [warning, setWarning] = useState(false);
+
+    const showWarning = () => {
+        if (!localStorage.getItem('user_id')) {
+            setWarning(true);
+        }
+    }
+
+    const hideWarning = () => {
+        setWarning(false);
+    }
+
     return (
-        <nav className="sticky z-[999] top-0 flex justify-between items-center backdrop-blur px-16 py-8">
-            <Logo />
-            <ul className="flex space-x-8 font-semibold">
-                <li>
-                    <Link to={"/"} className="text-GF-grey hover:font-extrabold focus:font-extrabold focus:text-GF-green">Beranda</Link>
-                </li>
-                <li>
-                    <Link to={"/Discover"} className="text-GF-grey hover:font-extrabold focus:font-extrabold focus:text-GF-green">Temukan</Link>
-                </li>
-                <li>
-                    <Link to={"/News"} className="text-GF-grey hover:font-extrabold focus:font-extrabold focus:text-GF-green">Berita</Link>
-                </li>
-                <li>
-                    <Link to={"/Data"} className="text-GF-grey hover:font-extrabold focus:font-extrabold focus:text-GF-green">Data</Link>
-                </li>
-            </ul>
-            <RightNavBarComponent />
-        </nav>
+        <>
+            <div className={`${warning === false ? 'hidden' : ''} z-[999] fixed shadow m-auto top-0 bottom-0 left-0 right-0 w-1/4 h-fit bg-white rounded-3xl p-8`}>
+                <div className="flex justify-end">
+                    <button onClick={hideWarning} className="text-2xl">&times;</button>
+                </div>
+                <p className="my-4 text-justify">Untuk menggunakan fitur <span className="font-bold text-GF-green">Temukan Makanan</span>, silahkan masuk terlebih dahulu menggunakan akun Anda.</p>
+                <Link to={'/SignIn'}>
+                    <button className="w-full py-4 bg-GF-green text-white rounded-xl transition hover:bg-opacity-75">
+                        Masuk
+                    </button>
+                </Link>
+            </div>
+            {
+                warning === true ? <div className="z-[700] fixed backdrop-blur w-screen h-screen" /> : null
+            }
+            <nav className="sticky z-[800] top-0 flex justify-between items-center backdrop-blur px-16 py-8">
+                <Logo />
+                <ul className="flex space-x-8 font-semibold">
+                    <li>
+                        <Link to={"/"} className="text-GF-grey hover:font-extrabold focus:font-extrabold focus:text-GF-green">Beranda</Link>
+                    </li>
+                    <li onClick={showWarning}>
+                        <Link to={localStorage.getItem('user_id') ? "/Discover" : ""} className="text-GF-grey hover:font-extrabold focus:font-extrabold focus:text-GF-green">Temukan</Link>
+                    </li>
+                    <li>
+                        <Link to={"/News"} className="text-GF-grey hover:font-extrabold focus:font-extrabold focus:text-GF-green">Berita</Link>
+                    </li>
+                    <li>
+                        <Link to={"/Data"} className="text-GF-grey hover:font-extrabold focus:font-extrabold focus:text-GF-green">Data</Link>
+                    </li>
+                </ul>
+                <RightNavBarComponent />
+            </nav>
+        </>
     );
 }
 
