3d3276f508cf3efb2038fa37b57788636ef4633b
ARIFIN YUNIANTA <arifinklaten26@gmail.com>
2023-07-07 10:55:41 +0700

    update env

--- src/components/GolekFoodsApp.js
+++ src/components/GolekFoodsApp.js
@@ -11,7 +11,7 @@ import Data from "./pages/Data";
 
 function GolekFoodsApp() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/build">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/discover" element={<Discover />} />
